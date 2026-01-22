b1e14274a743221bab030b911f80f3044d3ef0fb
carol__r <carolzlr28@gmail.com>
2026-01-22 18:30:03 -0300

    menu hamburger no header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  menuOpen = false;
 
   ngAfterViewInit(): void {
     // força o Instagram a processar os embeds
     if ((window as any).instgrm) {
       (window as any).instgrm.Embeds.process();
     }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    document.body.style.overflow = this.menuOpen ? 'hidden' : '';
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    document.body.style.overflow = '';
   }
 }
