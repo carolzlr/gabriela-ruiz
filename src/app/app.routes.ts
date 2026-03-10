25734ad72a1b525d7b0838b3794da28c36d0b4f9
carol__r <carolzlr28@gmail.com>
2026-03-10 12:28:13 -0300

    ajuste das rotas para novos idiomas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HomeEnComponent } from './pages/home-en/home-en.component';
+import { HomeEsComponent } from './pages/home-es/home-es.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', component: HomeComponent }, // home em portugues
+  { path: 'en', component: HomeEnComponent }, // home em ingles
+  { path: 'es', component: HomeEsComponent }, // home em espanhol
+];
