f9f3946543c6135c444383aeec9ad5c9210c78c7
carol__r <carolzlr28@gmail.com>
2026-03-10 12:20:06 -0300

    criacao paginas em outros idiomas

---
+++ src/app/pages/home-en/home-en.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-en',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-en.component.html',
+  styleUrl: './home-en.component.scss'
+})
+export class HomeEnComponent {
+
+}
---
+++ src/app/pages/home-es/home-es.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-es',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-es.component.html',
+  styleUrl: './home-es.component.scss'
+})
+export class HomeEsComponent {
+
+}
